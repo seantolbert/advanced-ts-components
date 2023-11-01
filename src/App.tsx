289599e4f0456095844822aa4a97b7c7e01a9c1b
Sean Tolbert <seantolbert@seans-mac-mini.mynetworksettings.com>
2023-11-01 14:28:16 -0400

    demonstrates how to forward props to the input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import Button from "./components/Button";
-import Container from "./components/Container";
+import { useRef } from "react";
+import Input from "./components/Input";
 
 function App() {
+  const inputRef = useRef<HTMLInputElement>(null);
+
   return (
     <main>
-      <Container as={Button} type="button" />
+      <Input label="Test" id="test" ref={inputRef} />
     </main>
   );
 }
